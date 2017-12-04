a7c9996790a21217383dc99df50ac3f438a43a1f
Dênnis Dantas <ddspog@gmail.com>
2017-12-03 22:49:45 -0200

    docs(project): Add documentation of project package :memo:
    I've created the documentation of the project package. It will explain
    that uses generated go files that will store values fetched from the
    project itself.

---
+++ project/doc.go
@@ -0,0 +1,10 @@
+/*
+Package project it's a package for fetching data about the dino project.
+
+This package reads data generated from the build process of dino,
+generating go files, containing information stored in constants.
+
+It can be used calling its functions, all getters:
+
+	project.Version()
+*/package project
